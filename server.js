191b4a6a0496ce7a9c17ba9d365a16a587ed2c0c
TruKat <kati.k220@gmail.com>
2021-03-26 15:07:30 -0600

    deploying

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const fileUpload = require ('express-fileupload')
+const fileUpload = require("express-fileupload");
 const path = require("path");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
@@ -12,30 +12,27 @@ app.use(express.json());
 app.use(cors());
 
 app.use(fileUpload());
-app.post('/api/upload',(req,res)=>{
-    console.log('hello')
-  if(req.files===null){
-    return res.status(400).json({msg:'No file uploaded'})
+app.post("/api/upload", (req, res) => {
+  console.log("hello");
+  if (req.files === null) {
+    return res.status(400).json({ msg: "No file uploaded" });
   }
-  const file=req.files.file;
-  file.mv(`${__dirname}/client/public/uploads/${file.name}`,err=>{
-    if(err){
-      console.log(err)
-      return res.status(500).send(err)
+  const file = req.files.file;
+  file.mv(`${__dirname}/client/public/uploads/${file.name}`, (err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send(err);
     }
-    res.json({fileName:file.name,filePath:`/uploads/${file.name}`})
-  })
-  })
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
 
-  mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://localhost/GrooveIn',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
-    }
-  );
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/GrooveIn", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -51,5 +48,11 @@ app.use("/confirmation", require("./routes/confirmationRoutes"));
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 // });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`listening to http://localhost:${PORT}`));
